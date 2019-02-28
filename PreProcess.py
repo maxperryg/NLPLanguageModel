11ddf21cb154458f9aa6725017355051b7b06062
maxwellgrossman <maxperryg@gmail.com>
2019-02-27 21:08:12 -0500

    changed mad stuff, added bigram

--- PreProcess.py
+++ PreProcess.py
@@ -1,48 +1,84 @@
-def pre_process(train, brown_test, learner_test):
+import math
+
+# Add <s>'s and </s>'s where they belong
+# Lower case the whole thing
+def pre_process(corpus):
 
     # open the training file to read, assign the content variables
-    #   training_corpus
+    #   processed_corpus
     #   brown_test_corpus
     #   learner_test_corpus
-    training_corpus = open(train, "r").readlines()
-    brown_test_corpus = open(brown_test, "r").readlines()
-    learner_test_corpus = open(learner_test, "r").readlines()
+    processed_corpus = open(corpus, "r").readlines()
 
     # lowercase and pad all corpora with <s> and </s>
-    training_corpus = lower_and_pad(training_corpus)
-    brown_test_corpus = lower_and_pad(brown_test_corpus)
-    learner_test_corpus = lower_and_pad(learner_test_corpus)
+    processed_corpus = lower_and_pad(processed_corpus)
+
+    return processed_corpus
+    # brown_test_corpus = open(brown_test, "r").readlines()
+    # learner_test_corpus = open(learner_test, "r").readlines()
+
+
+
+    # brown_test_corpus = lower_and_pad(brown_test_corpus)
+    # learner_test_corpus = lower_and_pad(learner_test_corpus)
 
     # create dictionaries and fill it with all the tokens and frequencies of them
-    training_dictionary_without_unk = count(training_corpus)
-    brown_test_dictionary_without_unk = count(brown_test_corpus)
-    learner_test_dictionary_without_unk = count(learner_test_corpus)
+    # training_dictionary_without_unk = count(processed_corpus)
+    # brown_test_dictionary_without_unk = count(brown_test_corpus)
+    # learner_test_dictionary_without_unk = count(learner_test_corpus)
 
     #replace words only seen once with <unk>
-    training_dictionary_with_unk = augment_train_with_unk(training_dictionary_without_unk)
+    # training_dictionary_with_unk = augment_train_with_unk(training_dictionary_without_unk)
 
-    #relace any word seen in test but not train with unk and count unk
-    brown_test_dictionary_with_unk = augment_test_with_unk(training_dictionary_without_unk, brown_test_dictionary_without_unk)
-    learner_test_dictionary_with_unk = augment_test_with_unk(training_dictionary_without_unk, learner_test_dictionary_without_unk)
+    #relace any word seen in test but not corpus with unk and count unk
+    # brown_test_dictionary_with_unk = augment_test_with_unk(training_dictionary_without_unk, brown_test_dictionary_without_unk)
+    # learner_test_dictionary_with_unk = augment_test_with_unk(training_dictionary_without_unk, learner_test_dictionary_without_unk)
 
-    return \
-        {
-            "training":
-                {
-                    "without_unk": training_dictionary_without_unk,
-                    "with_unk": training_dictionary_with_unk
-                },
-            "brown_test":
-                {
-                    "without_unk": brown_test_dictionary_without_unk,
-                    "with_unk": brown_test_dictionary_with_unk
-                },
-            "learner_test":
-                {
-                    "without_unk": learner_test_dictionary_without_unk,
-                    "with_unk": learner_test_dictionary_with_unk
-                }
-        }
+
+
+    # return \
+    #     {
+    #         "training":
+    #             {
+    #                 "without_unk": training_dictionary_without_unk,
+    #                 "with_unk": training_dictionary_with_unk
+    #             },
+    #         "brown_test":
+    #             {
+    #                 "without_unk": brown_test_dictionary_without_unk,
+    #                 "with_unk": brown_test_dictionary_with_unk
+    #             },
+    #         "learner_test":
+    #             {
+    #                 "without_unk": learner_test_dictionary_without_unk,
+    #                 "with_unk": learner_test_dictionary_with_unk
+    #             }
+    #     }
+
+def lower_and_pad(array_of_sentences):
+    return "<s> " + " <s> ".join(array_of_sentences).replace("\n", " </s>").lower()
+
+
+def add_unk_to_training_corpus(corpus_without_unk):
+    dictionary = count(corpus_without_unk)
+    new_corpus = ""
+    for token in corpus_without_unk.split(" "):
+        if dictionary[token] != 1:
+            new_corpus += str(token) + " "
+        else:
+            new_corpus += "<unk> "
+    return new_corpus.rstrip()
+
+
+def add_unk_to_testing_corpus(training_corpus_without_unk, testing_corpus_without_unk):
+    dictionary = count(training_corpus_without_unk)
+    new_corpus = ""
+    for token in testing_corpus_without_unk.split(" "):
+        if token in dictionary:
+            new_corpus += str(token) + " "
+        else:
+            new_corpus += "<unk> "
+    return new_corpus.rstrip()
 
 
 def count(sentences):
@@ -57,36 +93,11 @@ def count(sentences):
     return dictionary
 
 
-def augment_train_with_unk(dictionary):
-    dictionary_with_unk = {}
-    dictionary_with_unk["<unk>"] = 0
-    for key in dictionary:
-        if dictionary[key] == 1:
-            dictionary_with_unk["<unk>"] += 1
-        else:
-            dictionary_with_unk[key] = dictionary[key]
-
-    return dictionary_with_unk
-
-
-def augment_test_with_unk(train, test):
-    dictionary_with_unk = {}
-    dictionary_with_unk["<unk>"] = 0
-    for key in test:
-        if train.has_key(key):
-            dictionary_with_unk[key] = test[key]
-        else:
-            dictionary_with_unk["<unk>"] += test[key]
-    return dictionary_with_unk
-
-
-def lower_and_pad(array_of_sentences):
-    return "<s> " + " <s> ".join(array_of_sentences).replace("\n", " </s>").lower()
-
 def calculate_percentage_types(test, train):
     word_types_in_test = len(test)
     word_types_in_test_not_in_train = len(set(test.keys()) - set(train.keys()))
     return float(word_types_in_test_not_in_train) / float(word_types_in_test) * 100
+
 
 def calculate_percentage_tokens(test, train):
     word_types_in_test_not_in_train = set(test.keys()) - set(train.keys())
@@ -94,38 +105,98 @@ def calculate_percentage_tokens(test, train):
     tokens_in_test = sum(test.values())
     for key in word_types_in_test_not_in_train:
         tokens_in_test_not_in_train+=test[key]
-    return float(tokens_in_test_not_in_train) / float(tokens_in_test) * 100
+    return tokens_in_test_not_in_train/tokens_in_test * 100
 
 
-all_dictionaries = pre_process("brown-train.txt", "brown-test.txt", "learner-test.txt")
+def calculate_unigram(training_corpus):
+    new = dict(training_corpus)
+    if "<s>" in new:
+        del new["<s>"]
+    total = sum(new.values())
+    ans = {key: math.log(value/total, 2) for key, value in new.items()}
+    return ans
 
+
+def calculate_bigram(training_corpus, training_dictionary):
+    text = training_corpus.split()
+    bigram = {}
+    for i in range(len(text[:-1])):
+        if text[i] not in bigram:
+            bigram[text[i]] = {}
+            bigram[text[i]][text[i+1]] = 1/training_dictionary[text[i]]
+        elif text[i + 1] not in bigram[text[i]]:
+            bigram[text[i]][text[i + 1]] = 1 / training_dictionary[text[i]]
+        else:
+            bigram[text[i]][text[i + 1]] += 1 / training_dictionary[text[i]]
+    return bigram
+
+def calculate_bigram_add_one(training_corpus, training_dictionary):
+    text = training_corpus.split()
+    bigram = {}
+    for i in range(len(text[:-1])):
+        if text[i] not in bigram:
+            bigram[text[i]] = {}
+            bigram[text[i]][text[i+1]] = 1/training_dictionary[text[i]]
+        elif text[i + 1] not in bigram[text[i]]:
+            bigram[text[i]][text[i + 1]] = 1 / training_dictionary[text[i]]
+        else:
+            bigram[text[i]][text[i + 1]] += 1 / training_dictionary[text[i]]
+    return bigram
+
+
+#all_dictionaries = pre_process("training.txt", "brown-test.txt", "learner-test.txt")
+
+brown_training_corpus_without_unk = pre_process("brown-train.txt")
+brown_testing_corpus_without_unk = pre_process("brown-test.txt")
+learner_testing_corpus_without_unk = pre_process("learner-test.txt")
+
+brown_training_corpus_with_unk = add_unk_to_training_corpus(brown_training_corpus_without_unk)
+brown_testing_corpus_with_unk = add_unk_to_testing_corpus(brown_training_corpus_without_unk, brown_testing_corpus_without_unk)
+learner_testing_corpus_with_unk = add_unk_to_testing_corpus(brown_training_corpus_without_unk, learner_testing_corpus_without_unk)
+
+brown_training_dictionary_without_unk = count(brown_training_corpus_without_unk)
+brown_testing_dictionary_without_unk = count(brown_testing_corpus_without_unk)
+learner_testing_dictionary_without_unk = count(learner_testing_corpus_without_unk)
+
+brown_training_dictionary_with_unk = count(brown_training_corpus_with_unk)
+brown_testing_dictionary_with_unk = count(brown_testing_corpus_with_unk)
+learner_testing_dictionary_with_unk = count(learner_testing_corpus_with_unk)
+
+unigram_of_training = calculate_unigram(brown_training_dictionary_with_unk)
+bigram_of_training = calculate_bigram(brown_training_corpus_with_unk, brown_training_dictionary_with_unk)
+
+print("done")
+
+print(sum(brown_training_dictionary_with_unk.values()))
 answers = open("Answers.txt", "w")
 
 answers.writelines("1) How many word types (unique words) are there in the training corpus? Please include "
                    "the padding symbols and the unknown token.\n\n"
-                   "Word types (unique words) in training corpus: " + str(len(all_dictionaries["training"]["with_unk"].keys())) + "\n\n")
+                   "Word types (unique words) in training corpus: " + str(len(brown_training_dictionary_with_unk.keys())) + "\n\n")
 
 answers.writelines("2) How many word tokens are there in the training corpus?\n\n"
-                   "Word tokens in training corpus: " + str(sum(all_dictionaries["training"]["with_unk"].values())))
+                   "Word tokens in training corpus: " + str(sum(brown_training_dictionary_with_unk.values())))
 
 answers.writelines("3) What percentage of word tokens and word types in each of the test corpora did not"
                    "occur in training (before you mapped the unknown words to <unk> in training and test data)?\n\n")
 
 answers.writelines("Percentage of word types in brown test corpus that are not in training: "
-                   + str(calculate_percentage_types(all_dictionaries["brown_test"]["without_unk"], all_dictionaries["training"]["without_unk"]))
+                   + str(calculate_percentage_types(brown_testing_dictionary_without_unk, brown_training_dictionary_without_unk))
                    + "%\n\n")
 
 answers.writelines("Percentage of word tokens in brown test corpus that are not in training: "
-                   + str(calculate_percentage_tokens(all_dictionaries["brown_test"]["without_unk"], all_dictionaries["training"]["without_unk"]))
+                   + str(calculate_percentage_tokens(brown_testing_dictionary_without_unk, brown_training_dictionary_without_unk))
                    + "%\n\n")
 
 answers.writelines("Percentage of word types in learner test corpus that are not in training: "
-                   + str(calculate_percentage_types(all_dictionaries["learner_test"]["without_unk"], all_dictionaries["training"]["without_unk"]))
+                   + str(calculate_percentage_types(learner_testing_dictionary_without_unk, brown_training_dictionary_without_unk))
                    + "%\n\n")
 
 answers.writelines("Percentage of word tokens in learner test corpus that are not in training: "
-                   + str(calculate_percentage_tokens(all_dictionaries["learner_test"]["without_unk"], all_dictionaries["training"]["without_unk"]))
+                   + str(calculate_percentage_tokens(learner_testing_dictionary_without_unk, brown_training_dictionary_without_unk))
                    + "%\n\n")
+
+
 # wordtypes = len(dict["training_dictionary"])
 # wordtokens = sum(dict["training_dictionary"].values())
 #
